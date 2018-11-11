997718dbe4a77e91e66d7152da572d5cdc231ca1
Gordon Lin <gordonlin89@gmail.com>
2018-11-11 00:19:26 -0800

    Initial api for reading data

--- dev/user.clj
+++ dev/user.clj
@@ -1,11 +1,21 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :refer [refresh]]
+  (:require [clojure.repl :refer :all]
+            [clojure.tools.namespace.repl :refer [refresh]]
             [clojure.test :refer [run-all-tests]]
+            [clojure.spec.alpha :as spec]
+            [clojure.java.io :as io]
+            [clojure.spec.gen.alpha :as gen]
             [com.example.csv-service.data :as data]))
 
 (defn run-tests []
   (run-all-tests #"com\.example\.csv-service\..*-test"))
 
-(defn foo []
-  :foo)
+(defn gen-data []
+  (gen/generate (spec/gen ::data/csv-data)))
 
+
+
+(comment
+  (with-open [rdr (io/reader "data/sample_pipes.csv")]
+    (->> (data/read rdr #" \| ")
+         (doall))))
--- src/com/example/csv_service/data.clj
+++ src/com/example/csv_service/data.clj
@@ -1,5 +1,34 @@
-(ns com.example.csv-service.data)
+(ns com.example.csv-service.data
+  (:refer-clojure :exclude [read])
+  (:require [clojure.spec.alpha :as spec]
+            [clojure.java.io :as io]))
 
-(defn foo []
-  :foo)
+(spec/def ::record
+  (spec/cat :last-name string?
+            :first-name string?
+            :gender string?
+            :favorite-color string?
+            :date-of-birth string?))
+
+(spec/def ::header
+  #{["LastName" "FirstName" "Gender"
+     "FavoriteColor" "DateOfBirth"]})
+
+(spec/def ::csv-data
+  (spec/cat :header ::header
+            :data (spec/* (spec/spec ::record))))
+
+(defn parse-record
+  [rec]
+  (spec/conform ::record rec))
+
+(defn parse [data]
+  (spec/assert (first data) ::header)
+  (->> (drop 1 data)
+       (map parse-record)))
+
+(defn read [rdr sep]
+  (->> (line-seq rdr)
+       (map #(clojure.string/split % sep))
+       (parse)))
 
