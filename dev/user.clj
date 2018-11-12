eb063ea70432e1f6887bf30c83a17c0ae602df02
Gordon Lin <gordonlin89@gmail.com>
2018-11-12 00:37:19 -0800

    Server skeleton

--- dev/user.clj
+++ dev/user.clj
@@ -5,8 +5,10 @@
             [clojure.spec.alpha :as spec]
             [clojure.java.io :as io]
             [clojure.spec.gen.alpha :as gen]
+            [io.pedestal.http :as http]
             [com.example.csv-service.data :as data]
-            [com.example.csv-service.data.spec :as data.spec]))
+            [com.example.csv-service.data.spec :as data.spec]
+            [com.example.csv-service.server :as server]))
 
 (defn run-tests []
   (run-all-tests #"com\.example\.csv-service\..*-test"))
@@ -27,6 +29,18 @@
     (->> (gen-data)
          (data/to-lines " ")
          (data/write-lines w3))))
+
+(defonce server
+  (->> {:env          :dev
+        ::http/join?  false
+        ::http/routes #(deref #'server/routes)}
+       (merge server/service)
+       (http/default-interceptors)
+       (http/dev-interceptors)
+       (http/create-server)))
+
+(defn start []
+  (http/start server))
 
 (comment
   (with-open [rdr (io/reader "data/sample_pipes.csv")]
--- src/com/example/csv_service/server.clj
+++ src/com/example/csv_service/server.clj
@@ -1,3 +1,37 @@
-(ns com.example.csv-service.server)
+(ns com.example.csv-service.server
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
 
+(defonce state (atom nil))
+
+(defn hello-world [req]
+  {:status 200 :body "Hello World!"})
+
+;; TODO
+
+;; Server
+
+(def routes
+  (route/expand-routes
+   #{["/hello" :get hello-world :route-name :hello]
+     ["/records/gender" :get hello-world :route-name :gender]
+     ["/records/birthdate" :get hello-world :route-name :birthdate]
+     ["/records/name" :get hello-world :route-name :name]}))
+
+(def service
+  {::http/routes routes
+   ::http/type   :jetty
+   ::http/port   8080})
+
+(defonce server
+  (http/create-server service))
+
+(defn start []
+  (http/start server))
+
+(defn stop []
+  (http/stop server))
+
+(defn -main []
+  (start))
 
