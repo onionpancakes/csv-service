4b64661fed04172da38abd24d57374422f5e7486
Gordon Lin <gordonlin89@gmail.com>
2018-11-11 19:59:30 -0800

    Impl part1; writes, merge and -main

--- src/com/example/csv_service/data.clj
+++ src/com/example/csv_service/data.clj
@@ -2,7 +2,8 @@
   (:refer-clojure :exclude [read])
   (:require [clojure.spec.alpha :as spec]
             [clojure.spec.gen.alpha :as gen]
-            [clojure.string :refer [split join includes?]])
+            [clojure.string :refer [split join includes?]]
+            [clojure.java.io :as io])
   (:import [java.text SimpleDateFormat]
            [java.util TimeZone]))
 
@@ -62,4 +63,36 @@
             d (spec/explain-data ::csv-data lines)]
         (throw (ex-info s d))))
     (spec/conform ::csv-data lines)))
+
+(defn write [wtr sep data]
+  (doseq [line (spec/unform ::csv-data data)]
+    (doto wtr
+      (.write (join sep line))
+      (.newLine))))
+
+(defn merge-data [base & others]
+  (->> (mapcat :data others)
+       (update base :data into)))
+
+(defn -main []
+  (.mkdir (io/file "./out"))
+  (with-open [r1 (io/reader "data/random_pipes.csv")
+              r2 (io/reader "data/random_comma.csv")
+              r3 (io/reader "data/random_space.csv")
+              w1 (io/writer "out/random_gender_last.csv")
+              w2 (io/writer "out/random_dob.csv")
+              w3 (io/writer "out/random_last.csv")]
+    (let [m (merge-data (read r1 #" \| ")
+                        (read r2 #", ")
+                        (read r3 #" "))]
+      (->> (partial sort-by (juxt :gender :last-name))
+           (update m :data)
+           (write w1 ","))
+      (->> (partial sort-by :date-of-birth)
+           (update m :data)
+           (write w2 ","))
+      (->> (partial sort-by :last-name)
+           (comp reverse)
+           (update m :data)
+           (write w3 ",")))))
 
