648e3e59b501e98b1c69b44453d0b0dc156375ca
Gordon Lin <gordonlin89@gmail.com>
2018-11-18 19:52:23 -0800

    Property tests for sorted endpoints

--- test/com/example/csv_service/server_test.clj
+++ test/com/example/csv_service/server_test.clj
@@ -133,13 +133,18 @@
 
 ;; Property tests
 
+;; Note: Property tests seems to run concurrently.
+;; Due to shared atom state, locking is nessessary.
+;; TODO: figure out lockless tests?
+
 (defn property-identity
   [sep csv-data]
-  (reset! state data-empty)
-  (post sep csv-data)
-  [(-> (get-gender) :body conform-body)
-   (-> (get-dob) :body conform-body)
-   (-> (get-name) :body conform-body)])
+  (locking state
+    (reset! state data-empty)
+    (post sep csv-data)
+    [(-> (get-gender) :body conform-body)
+     (-> (get-dob) :body conform-body)
+     (-> (get-name) :body conform-body)]))
 
 (spec/fdef property-identity
   :args (spec/cat :sep (set (keys supported-seps))
@@ -150,15 +155,70 @@
              (map set)
              (apply = (set (:data in))))))
 
+(defn property-sorted-gender
+  [sep init-st to-post]
+  (locking state
+    (reset! state init-st)
+    (doseq [csv-data to-post]
+      (post sep csv-data))
+    (-> (get-gender) :body conform-body :data)))
+
+(spec/fdef property-sorted-gender
+  :args (spec/cat :sep (set (keys supported-seps))
+                  :init-st ::ds/csv-data
+                  :to-post (spec/coll-of ::ds/csv-data :gen-max 8))
+  :ret (spec/cat
+        :female (spec/* (comp #{"Female"} :gender))
+        :male (spec/* (comp #{"Male"} :gender))))
+
+(defn property-sorted-dob
+  [sep init-st to-post]
+  (locking state
+    (reset! state init-st)
+    (doseq [csv-data to-post]
+      (post sep csv-data))
+    (->> (get-dob)
+         (:body)
+         (conform-body)
+         (:data)
+         (map :date-of-birth))))
+
+(spec/fdef property-sorted-dob
+  :args (spec/cat :sep (set (keys supported-seps))
+                  :init-st ::ds/csv-data
+                  :to-post (spec/coll-of ::ds/csv-data :gen-max 8))
+  :ret ds/ascending?)
+
+(defn property-sorted-name
+  [sep init-st to-post]
+  (locking state
+    (reset! state init-st)
+    (doseq [csv-data to-post]
+      (post sep csv-data))
+    (->> (get-name)
+         (:body)
+         (conform-body)
+         (:data)
+         (map :last-name))))
+
+(spec/fdef property-sorted-name
+  :args (spec/cat :sep (set (keys supported-seps))
+                  :init-st ::ds/csv-data
+                  :to-post (spec/coll-of ::ds/csv-data :gen-max 8))
+  :ret ds/descending?)
+
 ;; Test specced fns
 
 (alias 'stc 'clojure.spec.test.check)
 
 (def check-fns
-  [`property-identity])
+  [`property-identity
+   `property-sorted-gender
+   `property-sorted-dob
+   `property-sorted-name])
 
 (def check-opts
-  {::stc/opts {:num-tests 100}})
+  {::stc/opts {:num-tests 40}})
 
 (deftest test-specced-fns
   (-> (stest/check check-fns check-opts)
